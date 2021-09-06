22bc9a15c8f8aef281c799a80d2ed5d28973b39a
kkopec <s15509@pjwstk.edu.pl>
2021-09-06 16:00:14 +0200

    Cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
                             Photo Album
                         </Typography>
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                            Add your favourite photos to your own album! xyz
+                            Add your favourite photos to your own album!
                         </Typography>
                         <div className={classes.buttonsGrid}>
                             <Grid container spacing={2} justify="center">
